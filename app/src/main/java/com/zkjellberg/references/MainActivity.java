02e9dd6bf2808aa076cff8ac65c4c900e68973c1
Zachary Kjellberg <zach.kjellberg@gmail.com>
2018-11-27 20:04:57 -0500

    Updated Repo for compatibility with latest Android build environment

--- app/src/main/java/com/zkjellberg/references/MainActivity.java
+++ app/src/main/java/com/zkjellberg/references/MainActivity.java
@@ -1,7 +1,7 @@
 package com.zkjellberg.references;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,11 +11,10 @@ import com.zkjellberg.references.fragments.FragmentDash;
 import com.zkjellberg.references.fragments.FragmentExpandListView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
